bcfa2dd457d439bd106754dbfcd7468ef124c965
Zukhra <zuhruschka@mail.ru>
2023-01-03 18:16:32 +0400

    Initial commit

---
+++ Задача4.py
@@ -0,0 +1,19 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+
+original_list = [1,2,3,4,5,6,7,8,9]
+temp_list = []
+a = 8 #как запихнуть сюда размер листа?
+
+print(f'До:    {original_list}')
+
+while len(original_list) > 0:
+    x = random.randint(0, a)
+    temp_list.append(original_list[x])
+    original_list.pop(x)
+    a -= 1
+
+original_list = temp_list
+
+print(f'После: {original_list}')
